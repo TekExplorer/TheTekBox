5faea7077a2f15a93cf0103945cf042e4fc5dcbe
Omar Kamel <tekexplorerm@gmail.com>
2026-08-17 13:03:42 -0400

    Initial version

---
+++ OneMaxHpModifierCode/MainFile.cs
@@ -0,0 +1,28 @@
+using Godot;
+using HarmonyLib;
+using Logger = MegaCrit.Sts2.Core.Logging.Logger;
+using MegaCrit.Sts2.Core.Modding;
+using MegaCrit.Sts2.Core.Logging;
+using BaseLib.Extensions;
+using System.Reflection;
+
+namespace OneMaxHpModifier.OneMaxHpModifierCode;
+
+//You're recommended but not required to keep all your code in this package and all your assets in the OneMaxHpModifier folder.
+[ModInitializer(nameof(Initialize))]
+public partial class MainFile : Node
+{
+    public const string ModId = "OneMaxHpModifier"; //At the moment, this is used only for the Logger and harmony names.
+
+    public static Logger Logger { get; } = new(ModId, LogType.Generic);
+
+    public static void Initialize()
+    {
+        //If you want to use scripts defined in your mod for Godot scenes, uncomment the following line.
+        //Godot.Bridge.ScriptManagerBridge.LookupScriptsInAssembly(Assembly.GetExecutingAssembly());
+
+        Harmony harmony = new(ModId);
+
+        harmony.TryPatchAll(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ OneMaxHpModifierCode/Modifiers/Precarious.cs
@@ -0,0 +1,73 @@
+using System.Reflection;
+using System.Reflection.Emit;
+using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using BaseLib.Utils.Patching;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Runs;
+
+namespace OneMaxHpModifier.OneMaxHpModifierCode.Modifiers;
+
+[HarmonyPatch(typeof(CreatureCmd), nameof(CreatureCmd.GainMaxHp), MethodType.Async)]
+static class MaxHpPatch
+{
+    [HarmonyTranspiler]
+    static internal IEnumerable<CodeInstruction> Transpiler(ILGenerator generator, IEnumerable<CodeInstruction> code, MethodBase original)
+    {
+        return AsyncMethodCall.Create(generator, code, original,
+            callMethod: AccessTools.Method(typeof(MaxHpPatch), nameof(FixMaxHp)),
+            afterState: original
+        );
+    }
+
+    static internal async Task FixMaxHp(Creature creature)
+    {
+        if (!creature.IsPlayer) return;
+        var player = creature.Player!;
+        if (!player.RunState.Modifiers.Any(mod => mod is Precarious)) return;
+        await Precarious.LoseMaxHpToOne(creature);
+    }
+}
+
+public class Precarious : CustomModifierModel
+{
+    public override ModifierAlignment Alignment => ModifierAlignment.Bad;
+    protected override string IconPath => ImageHelperExtensions.GetModImagePath("modifiers/precarious.png");
+
+    public override Func<Task>? GenerateNeowOption(EventModel eventModel)
+    {
+        return () => LoseMaxHpToOne(eventModel.Owner.Creature);
+    }
+
+    internal static Task LoseMaxHpToOne(Creature creature)
+    {
+        return CreatureCmd.LoseMaxHp(new ThrowingPlayerChoiceContext(), creature, creature.MaxHp - 1, isFromCard: false);
+    }
+    protected override void AfterRunCreated(RunState runState)
+    {
+        runState.Players.Do(RemoveRelics);
+    }
+
+    static void RemoveRelics(Player player)
+    {
+        // player.RelicGrabBag.Remove<NutritiousOyster>();  // modify?
+        // player.RelicGrabBag.Remove<StoneHumidifier>();
+        // // remove byrd egg eats
+        // // Big mushroom
+        // // Mango??? // event
+        // // chosen cheese
+        // // Darkstone Periapt
+        // // Sere Talon
+
+        // player.RelicGrabBag.Remove<DragonFruit>();
+
+        // player.RelicGrabBag.Remove<Mango>();
+        // player.RelicGrabBag.Remove<Pear>();
+        // player.RelicGrabBag.Remove<Strawberry>();
+    }
+}
